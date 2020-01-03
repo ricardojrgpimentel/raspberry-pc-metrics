30366d417a9afbc6eae4a17dacdc375e29eb17db
Ricardo Pimentel <ricardojrgpimentel@gmail.com>
2020-01-03 11:40:47 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import Bg from './Bg'
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Bg>
+      <div className="circle-wrap">
+        <CircularProgressbar className='graph' value={66} text={`CPU ${66}%`} />
+        <CircularProgressbar className='graph' value={66} text={`CPU ${66}%`} />
+        <CircularProgressbar className='graph' value={66} text={`CPU ${66}%`} />
+        <CircularProgressbar className='graph' value={66} text={`CPU ${66}%`} />
+      </div>
+    </Bg>
+  )
 }
 
 export default App;
